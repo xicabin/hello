90844c389c8bac70173d0dac999476aa6c9c463f
Darcy <c.young@qq.com>
2016-10-12 15:04:36 +0800

    Add scaffold based add/edit

--- src/base-modules/db.js
+++ src/base-modules/db.js
@@ -20,9 +20,56 @@ const UserSchema = new Schema({
 	logined: Date,
 	created: Date
 })
+const users = mongoose.model('User', UserSchema)
+
+// Scaffold 属性
+// @autofill   function|string  自动计算属性，不需要用户输入
+// @showAs     enum             显示形式，支持 [ textarea, radio, select ]
+// @showName   string           显示名称，如果不指定，则显示字段名称
+// @showIndex  int              显示顺序，按照从小到大自顶向下排列
+
+const PageSchema = new Schema({
+	user_id: {
+		type: ObjectId,
+		required: true,
+		index: true,
+		autofill: (context) => context.myId()
+	},
+	title: {
+		type: String,
+		required: true,
+		showIndex: 1
+	},
+	body: {
+		type: String,
+		required: true,
+		showAs: 'textarea',
+		showIndex: 2
+	},
+	status: {
+		type: String,
+		required: true,
+		default: 'public',
+		enum: 'public private draft'.split(/\s+/),
+		showAs: 'radio',
+		showIndex: 3
+	},
+	updated: {
+		type: Date,
+		autofill: Date.now
+	}
+})
+const pages = mongoose.model('Page', PageSchema)
+pages.scaffoldName = 'pages'
+
+const scaffold = {}
+scaffold[pages.scaffoldName] = pages
 
 module.exports = {
 	UserSchema: UserSchema,
-	users: mongoose.model('User', UserSchema),
-	ObjectId: ObjectId
+	PageSchema: PageSchema,
+	users: users,
+	pages: pages,
+	ObjectId: ObjectId,
+	scaffold: scaffold
 }
---
+++ src/web-modules/scaffold.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const authz = require(__base + '/authz.js')
+const db = require(__base + '/db.js')
+
+function getFields(model) {
+	let fields = Object.assign({}, model.schema.paths)
+	Object.keys(fields).forEach(field => /^__/.test(field) && delete fields[field])
+
+	return fields
+}
+
+function getAutofill(context, model, field, fall) {
+	let def = model.schema.paths[field]
+	if (def && def.options.autofill !== undefined && def.options.autofill !== false) {
+		let autofill = def.options.autofill
+		return typeof(autofill) === 'function' ? autofill(context) : autofill
+	}
+	return fall
+}
+
+function* index(scaffoldName) {
+	// FIXME not implement, find pages by user_id with sort, pagination, filter and ...
+}
+
+function* saveAjax(scaffoldName) {
+	let model = db.scaffold[scaffoldName] || this.throw(`Unsupported model "${scaffoldName}"`, 400)
+
+	let body = this.request.body
+	let id = body._id
+	let fields = getFields(model)
+	delete fields._id
+	Object.keys(fields).forEach(field => fields[field] = getAutofill(this, model, field, body[field]))
+
+	let document = null
+	try {
+		if (id) {
+			document = yield model.findByIdAndUpdate(id, fields, { new: true, runValidators: true })
+		} else {
+			document = yield model.create(fields)
+		}
+	} catch (e) {
+		console.error(e)
+		this.throw(e.toString(), 400)
+	}
+	this.body = { result: true, document: document }
+}
+
+function* add(scaffoldName) {
+	let model = db.scaffold[scaffoldName] || this.throw(`Unsupported model "${scaffoldName}"`, 400)
+
+	this.body = yield this.render('scaffold/save.jade', {
+		model: model,
+		fields: getFields(model)
+	})
+}
+
+function* edit(scaffoldName, id) {
+	let model = db.scaffold[scaffoldName] || this.throw(`Unsupported model "${scaffoldName}"`, 400)
+
+	let fields = getFields(model)
+	let document = null
+	try {
+		document = yield model.findOne({ _id: id, user_id: this.myId() })
+	} catch (e) {
+		console.error(e)
+	}
+
+	if (!document) {
+		return this.throw(`"${id}" is not editable`, 400)
+	}
+
+	this.body = yield this.render('scaffold/save.jade', {
+		model: model,
+		fields: fields,
+		document: document
+	})
+}
+
+module.exports = {
+	use: (app) => {
+		const R = require('koa-route')
+		app.use(R.post('/r/scaffold/:model/save', authz.withAuth(saveAjax)))
+		app.use(R.get('/scaffold/:model/index', authz.withAuth(index)))
+		app.use(R.get('/scaffold/:model/add', authz.withAuth(add)))
+		app.use(R.get('/scaffold/:model/edit/:id', authz.withAuth(edit)))
+	}
+}
